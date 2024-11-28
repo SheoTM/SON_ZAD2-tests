bfe88d544c0163deab52eb591bbb07c079ec1bee
Leon <leon0411@op.pl>
2024-11-28 01:22:04 +0100

    Fixing the addStudent.py file

--- GitProject/addStudent.py
+++ GitProject/addStudent.py
@@ -1,23 +1,61 @@
-import pytest
-from unittest.mock import patch
-from attendance import Attendance  # Zakładając, że masz klasę Attendance w pliku attendance.py
+from exportStudents import ExportStudents
+import os
 
 
-class TestAttendance:
+class ModifyStudents:
+    # method to add student to list and export it to csv and txt files, files are deleted after operation and new list overwrite file
+    @staticmethod
+    def add_student_and_export(path, path2, students):
+        name = input("Enter student's name: ")
+        surname = input("Enter student's surname: ")
+        student_id = input("Enter student's ID: ")
+        students.append({"Name": name, "Surname": surname, "ID": student_id})
+        ExportStudents.csv(path, students)
+        ExportStudents.txt(path2, students)
 
-    @patch('builtins.input', side_effect=["1", "1"])  # Symulujemy dwukrotne wprowadzenie 1 (dla obecności)
-    def test_modify_attendance(self, mock_input):
-        # Given
-        attendance = Attendance()
-        attendance.presence = {
-            "2022-12-12": {"Kapa Boczek": True, "Anna Nowak": False}
-        }
-        date = "2022-12-12"
+    # method to add student to file, if file doesn't exist, it will create it
+    @staticmethod
+    def add_student_by_overwriting(path, path2):
+        student = [input("Enter student's name: "), input("Enter student's surname: "), input("Enter student's ID: ")]
+        if os.path.exists(path) and os.path.exists(path2):
+            file = open(path, "a")
+            file2 = open(path2, "a")
+            file.write("\n" + ";".join(student))
+            file2.write("\n" + student[0] + " " + student[1] + " - " + student[2])
+        else:
+            file = open(path, "a")
+            file2 = open(path2, "a")
+            file.write(";".join(student))
+            file2.write(student[0] + " " + student[1] + " - " + student[2])
 
-        # When
-        attendance.modify_attendance(date, "Kapa Boczek")
+    @staticmethod
+    def modify_student(path, path2, students):
+        student_id = input("Enter student's ID to modify: ")
+        for student in students:
+            if student["ID"] == student_id:
+                name = input("Enter new student's name: ")
+                surname = input("Enter new student's surname: ")
+                student["Name"] = name
+                student["Surname"] = surname
+                ExportStudents.csv(path, students)
+                ExportStudents.txt(path2, students)
+                return
+        print("Student not found.")
 
-        # Then
-        # Sprawdzamy tylko obecność Kapy Boczek
-        assert attendance.presence[date]["Kapa Boczek"] is True
-        # Usuwamy asercję dotyczącą Anny Nowak, ponieważ sprawdzamy tylko Kapa Boczek
+
+    @staticmethod
+    def delete_student(path, path2, students):
+          student_id = input("Enter student's ID to delete: ")
+          for i in range(len(students)):
+              if students[i]["ID"] == student_id:
+                  del students[i]
+                  ExportStudents.csv(path, students)
+                  ExportStudents.txt(path2, students)
+                  return
+          print("Student not found.")
+
+
+
+
+
+
