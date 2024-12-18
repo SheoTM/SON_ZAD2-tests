57b90360273a5de8624c6e8775c0841d3ae44a79
Leon <leon0411@op.pl>
2024-12-18 18:09:52 +0100

    Code fix

--- unit_tests/test_add_export_student.py
+++ unit_tests/test_add_export_student.py
@@ -7,13 +7,11 @@ from git_project.methods.export_students import ExportStudents
 
 class TestModifyStudentsAndExport(unittest.TestCase):
     def setUp(self):
-        # Przygotowanie ścieżek do plików i listy studentów
         self.path_csv = "test_students.csv"
         self.path_txt = "test_students.txt"
         self.students = []
 
     def tearDown(self):
-        # Usuwanie plików testowych po każdym teście
         if os.path.exists(self.path_csv):
             os.remove(self.path_csv)
         if os.path.exists(self.path_txt):
@@ -25,29 +23,27 @@ class TestModifyStudentsAndExport(unittest.TestCase):
         expected_csv_content = "Anna;Nowak;ABC45"
         expected_txt_content = "Anna Nowak - ABC45"
 
-        # When: Wywołanie metody ModifyStudents.add_student_and_export
+        # When
         ModifyStudents.add_student_and_export(self.path_csv, self.path_txt, self.students)
 
-        # Then: Sprawdzenie listy studentów
+        # Then
         self.assertEqual(self.students, [{"Name": "Anna", "Surname": "Nowak", "ID": "ABC45"}])
 
-        # Sprawdzenie rzeczywistej zawartości pliku CSV (ignorując białe znaki)
         with open(self.path_csv, "r") as file:
             csv_content = file.read().strip()
         self.assertEqual(csv_content, expected_csv_content)
 
-        # Sprawdzenie rzeczywistej zawartości pliku TXT (ignorując białe znaki)
         with open(self.path_txt, "r") as file:
             txt_content = file.read().strip()
         self.assertEqual(txt_content, expected_txt_content)
 
     def test_export_students_directly(self):
-        # Given: Lista studentów
+        # Given
         students = [{"Name": "Anna", "Surname": "Nowak", "ID": "ABC45"}]
         expected_csv_content = "Anna;Nowak;ABC45"
         expected_txt_content = "Anna Nowak - ABC45"
 
-        # When: Wywołanie ExportStudents
+        # When
         ExportStudents.csv(self.path_csv, students)
         ExportStudents.txt(self.path_txt, students)
 
